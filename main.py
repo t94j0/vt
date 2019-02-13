136477ae8403600df2d3de758ef4a5a2ad3c7ac6
Max Harley <maxh@maxh.io>
2019-02-12 21:39:14 -0500

    Comitted for life

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys
 import re
+import configparser
+from functools import partial
+from os import path
 from virus_total_apis import PublicApi as VirusTotalPublicApi
-
-API_KEY = ''
-vt = VirusTotalPublicApi(API_KEY)
 
 
 def is_md5(hsh):
@@ -22,7 +22,7 @@ def is_valid_hash(hsh):
     return is_md5(hsh) or is_sha1(hsh) or is_sha256(hsh)
 
 
-def virus_total(hsh):
+def has_positives(hsh):
     response = vt.get_file_report(hsh)
     if 'results' not in response:
         return None
@@ -31,22 +31,30 @@ def virus_total(hsh):
     return response['results'] if response['results']['positives'] > 0 else None
 
 
-def lookup(hsh):
+def vt_checker(vt: 'VirusTotalPublicApi', hsh: str):
     if not is_valid_hash(hsh):
         print('Error: {} is not a valid SHA1/SHA256/MD5 hash'.format(hsh))
         return
-    result = virus_total(hsh)
+    result = has_positives(hsh)
     if result is not None:
-        print(hsh)
+        yield result
 
 
-def clean_and_check(hsh):
-    hsh = hsh.strip()
-    lookup(hsh)
+def create_checker() -> 'VirusTotalPublicApi':
+    config = configparser.ConfigParser()
+    config.read([
+        'vt.conf',
+        path.expanduser('~/.vt.conf'),
+        path.expanduser('~/.config/vt.conf')
+    ])
+    api_key = config.get('config', 'API_KEY')
+    return VirusTotalPublicApi(api_key)
 
 
 def main():
-    [clean_and_check(hsh) for hsh in sys.stdin]
+    vt = create_checker()
+    for hsh in sys.stdin:
+        vt_checker(vt, hsh.strip())
 
 
 if __name__ == '__main__':
